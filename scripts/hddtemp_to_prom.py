ca26ff92301591fc172e15c501da441be7b61573
Martin Slabber <martin.slabber@gmail.com>
2018-08-02 15:11:52 +0200

    Add the rack data to the metrics.

--- scripts/hddtemp_to_prom.py
+++ scripts/hddtemp_to_prom.py
@@ -8,6 +8,7 @@ import subprocess
 import tempfile
 import csv
 import datetime
+import configparser
 
 METRIC = "node_disk_temperature_celsius"
 
@@ -34,7 +35,16 @@ def prom_metric(metric_name, label_values, value, timestamp=False):
     return metric_str
 
 
-def hddtemp_to_prometheus_textfile(path, disk_map):
+def create_labels(config, **kwargs):
+    labels = dict(kwargs)
+    for key in ['rack-name', 'rack-u']:
+        value = config.get(key)
+        if value:
+            labels[key] = value
+    return labels
+
+
+def hddtemp_to_prometheus_textfile(path, disk_map, config):
 
     fh, temp_file = tempfile.mkstemp(text=True)
     with open(temp_file, 'w') as fh:
@@ -45,7 +55,7 @@ def hddtemp_to_prometheus_textfile(path, disk_map):
         for bayno, disk_path in disk_map.items():
             abs_path = os.path.realpath(os.path.abspath(disk_path))
             temperature = get_hddtemp(abs_path)
-            labels = {'bayno': bayno, 'device': abs_path}
+            labels = create_labels(config, bayno=bayno, device=abs_path)
             fh.write(prom_metric(METRIC, labels, temperature))
             fh.write('\n')
     os.chmod(temp_file, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
@@ -64,6 +74,21 @@ def read_drive_map(map_file):
     return drive_map
 
 
+def create_config():
+    """Collects some system information."""
+    data = {}
+    if os.path.isfile('/etc/hardware/rack'):
+        config = configparser.ConfigParser()
+        config.read_file(open('/etc/hardware/rack'))
+
+        for section, key in [('rack', 'name'), ('rack', 'u')]:
+            value = config.get(section, key, fallback=None)
+            if value:
+                data[section + '-' + key] = value
+
+    return data
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('mapfile', nargs=1, type=argparse.FileType('r'),
@@ -76,4 +101,5 @@ if __name__ == "__main__":
     assert os.path.isdir(path), "Prometheus path do not exists."
 
     bays = read_drive_map(args.mapfile[0])
-    hddtemp_to_prometheus_textfile(path, bays)
+    config = create_config()
+    hddtemp_to_prometheus_textfile(path, bays, config)
